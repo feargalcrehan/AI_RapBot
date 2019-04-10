7886b800b66ab9934cb862d6a2cba97dd3ffe746
feargaljjcrehan <feargaljjcrehan@gmail.com>
2019-04-10 22:47:23 +0100

    * EmptyCSharpFile.cs: Create Solution with Selenium for web scraping
    * Test_Feature_File.feature:
    * Test_Feature_File.feature.cs:

--- AI_RapBot/EmptyCSharpFile.cs
+++ AI_RapBot/EmptyCSharpFile.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+            IWebDriver webDriver = new FirefoxDriver();
+
+
+        }
+    }
+}
